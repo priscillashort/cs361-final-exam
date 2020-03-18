a098b9da153bebd820cedfb5e7d5575efcecabad
Priscilla Short <pshort2@cocc.edu>
2020-03-18 11:30:39 -0700

    Create common interface between bikes references #1

--- bmx_bike.rb
+++ bmx_bike.rb
@@ -3,7 +3,7 @@ require_relative 'tail_pack'
 class BmxBike
 
   def initialize
-    @tail_pack = TailPack.new
+    @luggage = TailPack.new
     @weekly_price = 70
     @daily_price = 20
     @hourly_price = 5
@@ -17,8 +17,8 @@ class BmxBike
     puts "Adjusting seat..."
   end
 
-  def tail_pack
-    @tail_pack
+  def luggage
+    @luggage
   end
 
   def weekly_rate
